992fa6bb846fb1dd187ac2bd123a8072f96cd504
duxianghua <xianghua.du@stargt.com.cn>
2017-12-18 18:52:22 +0800

    Init commit

--- web/urls.py
+++ web/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index.as_view()),
-    url(r'^minions/([.*])', views.minions.as_view()),
+    url(r'^minions/(?P<mid>\w+)', views.minions.as_view()),
 ]
