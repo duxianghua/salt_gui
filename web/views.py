63c6203469efc2e0cc407287822c066d1cc12718
duxianghua <xianghua.du@stargt.com.cn>
2017-12-18 18:31:39 +0800

    Init commit

--- web/views.py
+++ web/views.py
@@ -1,24 +1,6 @@
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
-from rest_framework.permissions import IsAdminUser
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from .SaltAPI import SaltBaseViewSet
 
-
-class ViewSet(APIView):
-    authentication_classes = (SessionAuthentication, BasicAuthentication, TokenAuthentication)
-    permission_classes = (IsAdminUser,)
-    http_method_names = ['get', 'post']
-    content = {
-        'status': 'ok'
-    }
-
-    def get(self, request, *args, **kwargs):
-        return Response(self.content)
-
-    def post(self, request, *args, **kwargs):
-        return Response(self.content)
-
-
-class aaa(ViewSet):
-    def post(self, request, *args, **kwargs):
-        return Response(self.content)
+class aaa(SaltBaseViewSet):
+    '''
+    aaa
+    '''
