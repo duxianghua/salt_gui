42e8efddbe04a6b359e8f822bfff8039be1c9984
duxianghua <xianghua.du@stargt.com.cn>
2017-12-27 19:15:11 +0800

    Init commit

--- saltapi/netapi/keys.py
+++ saltapi/netapi/keys.py
@@ -10,12 +10,18 @@ def list_all(*args, **kwargs):
 
 def accept(minions, **kwargs):
     fun = 'key.accept'
+    if not isinstance(minions, list):
+        raise ValueError('minions must be formatted as a list')
     return api.wheel(fun, arg=minions)
 
 def delete(minions, **kwargs):
     fun = 'key.delete'
+    if not isinstance(minions, list):
+        raise ValueError('minions must be formatted as a list')
     return api.wheel(fun, arg=minions)
 
 def reject(minions, **kwargs):
     fun = 'key.reject'
+    if not isinstance(minions, list):
+        raise ValueError('minions must be formatted as a list')
     return api.wheel(fun, arg=minions)
